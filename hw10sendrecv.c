c9828cfccfd27120ab08cb3bd300bbcf37a1ab70
Tony <adeglio@gmail.com>
2015-12-06 15:51:25 -0500

    send/rcv working

--- hw10sendrecv.c
+++ hw10sendrecv.c
@@ -18,13 +18,14 @@ void print_lst();
 
 int main(int argc, char **argv) {
   int i,n,vector_x[NELMS],vector_y[NELMS];
-  int prod,tmp_prod,sidx,eidx,size;
+  int prod,tmp_prod,sidx,eidx,size,mprod;
   int pid,nprocs, rank;
   double stime,etime;
   MPI_Status status;
   MPI_Comm world;
 
   n = atoi(argv[1]);
+
   if (n > NELMS) { printf("n=%d > N=%d\n",n,NELMS); exit(1); }
 
   MPI_Init(&argc, &argv);
@@ -32,10 +33,33 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
+  size = n / nprocs;
+  sidx = pid * size;
+  eidx = sidx + size;
+
   stime = MPI_Wtime();
   // ...
   // ...
   // ...
+  
+  init_lst(vector_x, n);
+  init_lst(vector_y, n);
+
+  prod = dot_product(vector_x, vector_y, sidx, eidx);
+  printf("%d\n", prod);
+  
+  if (pid == MASTER) {
+      for (i = 1; i < nprocs; i++)
+	{
+	  MPI_Recv(&tmp_prod, 1, MPI_INT, i, 123, MPI_COMM_WORLD, &status);
+	  prod += tmp_prod;
+	}  
+    }
+
+  else {
+      MPI_Send(&prod, 1, MPI_INT, MASTER, 123, MPI_COMM_WORLD);
+    }
+
   etime = MPI_Wtime();
 
   if (pid == MASTER) {
@@ -45,10 +69,16 @@ int main(int argc, char **argv) {
   MPI_Finalize();
 }
 
-int dot_product(int s,int e){
+int dot_product(int *x, int *y, int sidx, int eidx){
   int i,prod=0;
   // ...
   // ...
+
+  for (i = sidx; i<eidx; i++)
+    {
+      prod += x[i] * y[i];
+    }
+
   return prod;
 }
 
